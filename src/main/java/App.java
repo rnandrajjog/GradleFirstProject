a837056904b7679342540e1a0289331c4bf2503e
Rakesh Nandrajog <r_nandrajog@yahoo.com>
2020-03-24 19:42:59 +0530

    Initial Commit

---
+++ src/main/java/App.java
@@ -0,0 +1,12 @@
+public class App {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world");
+        int i= 10;
+        int j=30;
+
+        System.out.println("The total is " + (i+j));
+
+    }
+}
